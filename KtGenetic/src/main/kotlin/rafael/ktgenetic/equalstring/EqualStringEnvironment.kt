fa4c94befb0cd8b334f6ad84028710ab1b6beff6
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-06-17 18:31:52 -0300

    Fitting into Utils helper methods

--- KtGenetic/src/main/kotlin/rafael/ktgenetic/equalstring/EqualStringEnvironment.kt
+++ KtGenetic/src/main/kotlin/rafael/ktgenetic/equalstring/EqualStringEnvironment.kt
@@ -1,7 +1,9 @@
 package rafael.ktgenetic.equalstring
 
 import rafael.ktgenetic.Environment
-import java.util.*
+import rafael.ktgenetic.createCutPositions
+import rafael.ktgenetic.geneticRandom
+import rafael.ktgenetic.makeCuttingIntoPieces
 
 class EqualStringEnvironment(val target: String,
                              override val generationSize: Int,
@@ -9,13 +11,11 @@ class EqualStringEnvironment(val target: String,
                              override val mutationFactor: Double = 0.01) :
         Environment<Char, Word> {
 
-    val random = Random()
-
     private val range = ' '.rangeTo('~') + 192.toChar().rangeTo(255.toChar())
 
     private val fitness: StringFitness = EqualCharsFitness()
 
-    private fun randomChar(): Char = range[random.nextInt(range.size)]
+    private fun randomChar(): Char = range[geneticRandom.nextInt(range.size)]
 
     private fun createRandomWord(): Word = Word(1.rangeTo(target.length).map { _ -> randomChar() }.joinToString(""))
 
@@ -24,25 +24,18 @@ class EqualStringEnvironment(val target: String,
                     .map { _ -> createRandomWord() }
 
     override fun executeMutation(sequence: List<Char>): List<Char> {
-        val mutationPoint = random.nextInt(sequence.size)
-        val mutatedGene: Char = range[random.nextInt(range.size)]
+        val mutationPoint = geneticRandom.nextInt(sequence.size)
+        val mutatedGene: Char = range[geneticRandom.nextInt(range.size)]
         val result = sequence.toMutableList()
         result[mutationPoint] = mutatedGene
 
         return result.toList()
     }
 
-    override fun getCutPositions(): Pair<Int, Int> {
-        val pos1 = 1 + random.nextInt(target.length - 2)
-        val pos2 = 1 + random.nextInt(target.length - 1 - pos1) + pos1
+    override fun getCutPositions(): Pair<Int, Int> = createCutPositions(target.length)
 
-        return Pair(pos1, pos2)
-    }
-
-    override fun cutIntoPieces(sequence: List<Char>, cutPositions: Pair<Int, Int>): Triple<List<Char>, List<Char>, List<Char>> = Triple(
-            sequence.subList(0, cutPositions.first),
-            sequence.subList(cutPositions.first, cutPositions.second),
-            sequence.subList(cutPositions.second, sequence.size))
+    override fun cutIntoPieces(sequence: List<Char>, cutPositions: Pair<Int, Int>): Triple<List<Char>, List<Char>, List<Char>> =
+            makeCuttingIntoPieces(sequence, cutPositions)
 
     override fun calculateFitness(sequence: List<Char>): Double = fitness.calculate(String(sequence.toCharArray()), target)
 
