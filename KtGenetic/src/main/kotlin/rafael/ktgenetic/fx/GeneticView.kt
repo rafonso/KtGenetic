d4975ab12c1bfcd9734ca0b130998d0d9c8efeee
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-07-01 19:43:42 -0300

    Added Combo for SelectionOperatorChoice.

--- KtGenetic/src/main/kotlin/rafael/ktgenetic/fx/GeneticView.kt
+++ KtGenetic/src/main/kotlin/rafael/ktgenetic/fx/GeneticView.kt
@@ -8,8 +8,9 @@ import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
+import javafx.util.StringConverter
+import rafael.ktgenetic.selection.SelectionOperatorChoice
 import tornadofx.*
-import java.awt.event.ActionEvent
 
 
 abstract class GeneticView(title: String) : View(title) {
@@ -18,18 +19,19 @@ abstract class GeneticView(title: String) : View(title) {
     private val values: ObservableList<Int> = FXCollections.observableArrayList(1, 10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
     private val mutationFactors: ObservableList<Double> = FXCollections.observableArrayList(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
 
-    val pnlInput: GridPane                          by fxid()
-    val pnlOutput: GridPane                         by fxid()
-    val pnlButtons: GridPane                        by fxid()
-    val cmbGenerations: ComboBox<Int>               by fxid()
-    val cmbPopulation: ComboBox<Int>                by fxid()
-    val cmbMutationFactor: ComboBox<Double>         by fxid()
-    val btnStop: Button                             by fxid()
-    val btnStart: Button                            by fxid()
-    val lblGeneration: Label                        by fxid()
-    val lblBestFitness: Label                       by fxid()
-    val lblTime: Label                              by fxid()
-    val lineChartFitness: LineChart<Int, Double>    by fxid()
+    val pnlInput: GridPane                                      by fxid()
+    val pnlOutput: GridPane                                     by fxid()
+    val pnlButtons: GridPane                                    by fxid()
+    val cmbGenerations: ComboBox<Int>                           by fxid()
+    val cmbPopulation: ComboBox<Int>                            by fxid()
+    val cmbMutationFactor: ComboBox<Double>                     by fxid()
+    val cmbSelectionOperator: ComboBox<SelectionOperatorChoice> by fxid()
+    val btnStop: Button                                         by fxid()
+    val btnStart: Button                                        by fxid()
+    val lblGeneration: Label                                    by fxid()
+    val lblBestFitness: Label                                   by fxid()
+    val lblTime: Label                                          by fxid()
+    val lineChartFitness: LineChart<Int, Double>                by fxid()
 
     init {
         cmbGenerations.items = values
@@ -39,6 +41,9 @@ abstract class GeneticView(title: String) : View(title) {
         cmbMutationFactor.items = mutationFactors
         cmbMutationFactor.value = 0.1
         lineChartFitness.yAxis.isTickMarkVisible = false
+        cmbSelectionOperator.items = FXCollections.observableArrayList(SelectionOperatorChoice.values().toList())
+        cmbSelectionOperator.converter = SelectionOperatorConverter()
+
     }
 
     private fun disableInputComponents(disable: Boolean) {
@@ -50,10 +55,19 @@ abstract class GeneticView(title: String) : View(title) {
     fun startProcessing() {
         println("START")
         disableInputComponents(true)
+        println(cmbSelectionOperator.value)
     }
 
     fun stopProcessing() {
         println("STOP")
         disableInputComponents(false)
     }
+}
+
+class SelectionOperatorConverter : StringConverter<SelectionOperatorChoice>() {
+
+    override fun toString(choice: SelectionOperatorChoice?): String = choice!!.description
+
+    override fun fromString(string: String?): SelectionOperatorChoice = SelectionOperatorChoice.values().first { it.description == string }
+
 }
