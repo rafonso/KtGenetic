7a2d02184075d3b4e5b7321534421f30cd02c811
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-07-02 11:25:34 -0300

    Method to add Component with its respective title

--- KtGenetic/src/main/kotlin/rafael/ktgenetic/fx/GeneticView.kt
+++ KtGenetic/src/main/kotlin/rafael/ktgenetic/fx/GeneticView.kt
@@ -2,11 +2,14 @@ package rafael.ktgenetic.fx
 
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
+import javafx.geometry.Orientation
+import javafx.scene.Node
 import javafx.scene.chart.LineChart
 import javafx.scene.control.Button
 import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.FlowPane
 import javafx.scene.layout.GridPane
 import javafx.util.StringConverter
 import rafael.ktgenetic.processor.GeneticProcessorChoice
@@ -19,8 +22,9 @@ abstract class GeneticView(title: String, val processorChoice: GeneticProcessorC
 
     private val values: ObservableList<Int> = FXCollections.observableArrayList(1, 10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
     private val mutationFactors: ObservableList<Double> = FXCollections.observableArrayList(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+    private val maxColumns = 5
 
-    protected val pnlInput: GridPane                                      by fxid()
+    protected val pnlInput: GridPane                                    by fxid()
     private val pnlOutput: GridPane                                     by fxid()
     private val pnlButtons: GridPane                                    by fxid()
     private val cmbGenerations: ComboBox<Int>                           by fxid()
@@ -34,7 +38,8 @@ abstract class GeneticView(title: String, val processorChoice: GeneticProcessorC
     private val lblTime: Label                                          by fxid()
     private val lineChartFitness: LineChart<Int, Double>                by fxid()
 
-
+    private var lastRow = 1
+    private var lastColumn = 0
 
     init {
         println("GeneticView - init start")
@@ -55,6 +60,22 @@ abstract class GeneticView(title: String, val processorChoice: GeneticProcessorC
         btnStart.isDisable = disable
         btnStop.isDisable = !disable
         pnlInput.children.filtered { it != pnlButtons }.forEach { it.isDisable = disable }
+    }
+
+    protected fun addComponent(title: String, component: Node, colspan: Int = 1) {
+        assert(colspan <= 5) // , () -> {"Colspan must be at least 5. It was $colspan"})
+        
+        val panel = FlowPane(Orientation.VERTICAL)
+        panel.styleClass.add("panel-control")
+        panel.add(Label(title))
+        panel.add(component)
+        
+        if(lastColumn + colspan > maxColumns) {
+            lastRow += 1
+            lastColumn = 0
+        }
+        pnlInput.add(panel, lastColumn, lastRow, colspan, 1)
+        lastColumn += colspan
     }
 
     fun startProcessing() {
