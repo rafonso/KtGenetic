f9ec2dcebed4ef415fed53ee99290b27414a5766
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-07-02 11:36:38 -0300

    Width Adjusts. Removing Genetic Node.

--- KtGenetic/src/test/kotlin/rafael/ktgenetic/fx/GeneticViewTEst.kt
+++ KtGenetic/src/test/kotlin/rafael/ktgenetic/fx/GeneticViewTEst.kt
@@ -1,10 +1,9 @@
 package rafael.ktgenetic.fx
 
 import javafx.collections.FXCollections
+import javafx.scene.control.CheckBox
 import javafx.scene.control.ComboBox
-import javafx.scene.control.Label
 import javafx.scene.control.TextField
-import javafx.scene.layout.GridPane
 import rafael.ktgenetic.processor.GeneticProcessorChoice
 import tornadofx.*
 
@@ -14,17 +13,13 @@ class GeneticViewTest : GeneticView("TESTE", GeneticProcessorChoice.SIMPLE) {
 
     private val txfValor = TextField()
     private val cmbValores = ComboBox<Int>(FXCollections.observableArrayList(1, 2, 3, 4))
-/*
-    private val node: GeneticNode by lazy {
-        GeneticNode("ALFA:", TextField("TESTE"))
 
-    }
-*/
     init {
-        val nodeText = GeneticNode("Text:", txfValor)
-        pnlInput.add(nodeText,0, 1)
-        val nodeCombo = GeneticNode("Value:", cmbValores)
-        pnlInput.add(nodeCombo, 1, 1)
+        txfValor.prefWidth = 400.0
+        addComponent("Text", txfValor, 2)
+        addComponent("Value:", cmbValores)
+        addComponent("Jumping row", TextField(), 4)
+        addComponent("It should be at end of row", CheckBox())
     }
 
 
