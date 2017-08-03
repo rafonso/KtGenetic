86a3ee40991236c2954d0a355645a73a5e69f748
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-08-03 20:15:20 -0300

    test for canvas inside borderpane.
    code extracted from
    https://stackoverflow.com/questions/37678704/how-to-embed-javafx-canvas-into-borderpane

---
+++ track/src/test/kotlin/CanvasLauncher.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CanvasLauncher{
+
+    public static void main(String[] args) {
+        Application.launch(CanvasInBorderPane.class, args);
+    }
+
+}
