1386b68962d85c7d7a6a83f29f836b560ec5f4de
Rafael Ubiratam Clemente Afonso <rafonso@gmail.com>
2017-08-05 06:15:40 -0300

    Icon

--- KtGenetic/src/main/kotlin/rafael/ktgenetic/fx/GeneticView.kt
+++ KtGenetic/src/main/kotlin/rafael/ktgenetic/fx/GeneticView.kt
@@ -12,6 +12,7 @@ import javafx.scene.control.Alert
 import javafx.scene.control.Button
 import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
+import javafx.scene.image.Image
 import javafx.scene.input.MouseEvent
 import javafx.scene.input.ScrollEvent
 import javafx.scene.layout.BorderPane
