de898660ccd48188de1c16ecd06788b03b11cbe6
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2019-11-06 13:30:41 -0300

    Refactorings

--- ktgenetic-pictures_comparsion/src/main/kotlin/rafael/ktgenetic/pictures_comparsion/ScreenEnvironment.kt
+++ ktgenetic-pictures_comparsion/src/main/kotlin/rafael/ktgenetic/pictures_comparsion/ScreenEnvironment.kt
@@ -2,29 +2,28 @@ package rafael.ktgenetic.pictures_comparsion
 
 import rafael.ktgenetic.Environment
 import rafael.ktgenetic.*
+import kotlin.math.sqrt
 
-class ScreenEnvironment(private val originalPicture: Pixels,
-                        private val width: Int, private val height: Int,
-                        override val maxGenerations: Int = Int.MAX_VALUE,
-                        override val generationSize: Int = 10,
-                        override var mutationFactor: Double = 0.01) : Environment<Bitmap, Screen> {
+class ScreenEnvironment(
+    private val originalBitmaps: Array<Array<Triple<Int, Int, Int>>>,
+    coverage: Double,
+    override val maxGenerations: Int = Int.MAX_VALUE,
+    override val generationSize: Int = 10,
+    override var mutationFactor: Double = 0.01
+) : Environment<Bitmap, Screen> {
+
+    private val generator = PixelsGenerator(originalBitmaps.size, originalBitmaps[0].size, coverage)
+
+    private fun randomByte() = randomIntInclusive(255)
 
     override fun getFirstGeneration(): List<Screen> =
-            (0..generationSize).map {
-                Screen(
-                        (0..width).flatMap { x ->
-                            (0..height).map { y ->
-                                Bitmap(x, y, geneticRandom.nextInt(256), geneticRandom.nextInt(256), geneticRandom.nextInt(256))
-                            }
-                        })
-            }
+        (0..generationSize).map { Screen(generator.createBitmaps()) }
 
-
-    override fun getCutPositions(): Pair<Int, Int> = createCutPositions(originalPicture.size)
+    override fun getCutPositions(): Pair<Int, Int> = createCutPositions(generator.bitmapsSize)
 
     override fun executeMutation(sequence: List<Bitmap>): List<Bitmap> {
         val mutationPos = geneticRandom.nextInt(sequence.size)
-        val mutationPixel = sequence[mutationPos].copy(r = geneticRandom.nextInt(256), g = geneticRandom.nextInt(256), b = geneticRandom.nextInt(256))
+        val mutationPixel = sequence[mutationPos].copy(r = randomByte(), g = randomByte(), b = randomByte())
 
         return sequence.replace(mutationPos, mutationPixel)
     }
@@ -32,9 +31,21 @@ class ScreenEnvironment(private val originalPicture: Pixels,
     override fun createNewChromosome(sequence: List<Bitmap>): Screen = Screen(sequence)
 
     override fun calculateFitness(chromosome: Screen): Double {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun resultFound(genotypes: List<Screen>): Boolean = (genotypes[0].distance() == 0.0)
+        fun calculateBitmapDistance(bitmap: Bitmap) {
+            val pixel = originalBitmaps[bitmap.x][bitmap.y]
+
+            val deltaR = bitmap.r - pixel.first
+            val deltaG = bitmap.g - pixel.second
+            val deltaB = bitmap.b - pixel.third
+
+            val distanceSquare = (deltaR * deltaR) + (deltaG * deltaG) + (deltaB * deltaB)
+            bitmap.distance = sqrt(distanceSquare.toDouble())
+        }
+
+        chromosome.content.forEach(::calculateBitmapDistance)
+
+        return (255.0 - chromosome.distance) / 255
+    }
 
 }
